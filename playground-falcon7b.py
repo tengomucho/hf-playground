b9d5e2ae182cdd7cfdc398cbd32192e5a13b4b3f
Alvaro Moran <alvaro.moran@huggingface.co>
2024-01-29 12:48:52 +0000

    feat: added falcon7b

---
+++ playground-falcon7b.py
@@ -0,0 +1,30 @@
+from transformers import AutoTokenizer
+import transformers
+import torch
+
+# From https://huggingface.co/tiiuae/falcon-7b-instruct
+
+model = "tiiuae/falcon-7b-instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
+)
+
+assert pipeline.model.device.type == "cuda"
+
+sequences = pipeline(
+   "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Girafatron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
+    max_length=200,
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
